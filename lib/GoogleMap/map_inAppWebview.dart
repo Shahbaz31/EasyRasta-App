32eebf0f931c965890b879379e35eab9decd48d6
Shahbaz31 <138029937+Shahbaz31@users.noreply.github.com>
2024-07-22 22:02:51 +0500

    Update map_inAppWebview.dart

--- lib/GoogleMap/map_inAppWebview.dart
+++ lib/GoogleMap/map_inAppWebview.dart
@@ -1,55 +1,55 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter_inappwebview/flutter_inappwebview.dart';
-//
-// class MapWebView extends StatefulWidget {
-//   const MapWebView({Key? key}) : super(key: key);
-//
-//   @override
-//   State<MapWebView> createState() => _MapWebViewState();
-// }
-//
-// class _MapWebViewState extends State<MapWebView> {
-//   double _progress = 0;
-//   late InAppWebViewController inAppWebViewController;
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return SafeArea(
-//       child: PopScope(
-//         canPop:true,
-//         onPopInvoked: (bool didPop) async{
-//           //  onWillPop: () async {
-//
-//           return Future.value(true);
-//         },
-//         child: Scaffold(
-//           body: Stack(
-//             children: [
-//               InAppWebView(
-//                 initialUrlRequest: URLRequest(
-//                   url: WebUri('https://www.google.co.in/maps/'),
-//                 ),
-//                 onWebViewCreated: (InAppWebViewController controller) {
-//                   inAppWebViewController = controller;
-//                 },
-//                 onProgressChanged:
-//                     (InAppWebViewController controller, int progress) {
-//                   setState(() {
-//                     _progress = progress / 100;
-//                   });
-//                 },
-//               ),
-//               _progress < 1
-//                   ? Container(
-//                 child: LinearProgressIndicator(
-//                   value: _progress,
-//                 ),
-//               )
-//                   : SizedBox()
-//             ],
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
+import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+
+class MapWebView extends StatefulWidget {
+  const MapWebView({Key? key}) : super(key: key);
+
+  @override
+  State<MapWebView> createState() => _MapWebViewState();
+}
+
+class _MapWebViewState extends State<MapWebView> {
+  double _progress = 0;
+  late InAppWebViewController inAppWebViewController;
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: PopScope(
+        canPop:true,
+        onPopInvoked: (bool didPop) async{
+          //  onWillPop: () async {
+
+          return Future.value(true);
+        },
+        child: Scaffold(
+          body: Stack(
+            children: [
+              InAppWebView(
+                initialUrlRequest: URLRequest(
+                  url: WebUri('https://www.google.co.in/maps/'),
+                ),
+                onWebViewCreated: (InAppWebViewController controller) {
+                  inAppWebViewController = controller;
+                },
+                onProgressChanged:
+                    (InAppWebViewController controller, int progress) {
+                  setState(() {
+                    _progress = progress / 100;
+                  });
+                },
+              ),
+              _progress < 1
+                  ? Container(
+                child: LinearProgressIndicator(
+                  value: _progress,
+                ),
+              )
+                  : SizedBox()
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
